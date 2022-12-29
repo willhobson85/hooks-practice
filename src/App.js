3ba7ba740304de82f160f0b6532c6c14f00c5362
William Hobson <will.hobson85@gmail.com>
2022-12-29 15:19:41 -0500

    Use useState hook to create a google search box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from 'react'
+
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  }
+  
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery)
+    setUserQuery(event.target.value)
+  }
+
+
   return (
     <div className="App">
       <h1>Hello, Will!</h1>
+      <div className='form'>
+        <input 
+          value={userQuery} 
+          onChange={updateUserQuery} 
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
     </div>
   );
 }
